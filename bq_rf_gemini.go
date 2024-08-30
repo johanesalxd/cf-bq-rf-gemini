7c3ef5b8d5c41c1ca71219df5bf402bcda8ec61d
Jo Alex <17249308+alexanderv21@users.noreply.github.com>
2024-08-30 08:01:39 +0800

    using client pool to optimize connection

--- bq_rf_gemini.go
+++ bq_rf_gemini.go
@@ -3,13 +3,32 @@ package bqrfgemini
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"sync"
 
 	"github.com/google/generative-ai-go/genai"
 	"google.golang.org/api/option"
 )
+
+// TODO: Replace the API key with an environment variable for better security
+// TODO: Change from Google AI to Vertex AI
+// TODO: Update client initialization to use Vertex AI
+var clientPool = &sync.Pool{
+	New: func() interface{} {
+		client, err := genai.NewClient(context.Background(), option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
+		if err != nil {
+			log.Printf("Error creating new client: %v", err)
+			return nil
+		}
+
+		log.Print("Client created")
+
+		return client
+	},
+}
 
 // BQRFGemini handles HTTP requests for the BigQuery Remote Function using Gemini AI
 func BQRFGemini(w http.ResponseWriter, r *http.Request) {
@@ -23,19 +42,19 @@ func BQRFGemini(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithCancel(r.Context())
 	defer func() {
 		log.Print("Done, Goroutines closed")
+
 		cancel()
 	}()
 
-	// TODO: Replace the API key with an environment variable for better security
-	// TODO: Change from Google AI to Vertex AI
-	// TODO: Update client initialization to use Vertex AI
-	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
-	if err != nil {
-		SendError(w, err, http.StatusInternalServerError)
+	client := clientPool.Get().(*genai.Client)
+	if client == nil {
+		SendError(w, errors.New("failed to get client from pool"), http.StatusInternalServerError)
 
 		return
 	}
-	defer client.Close()
+	defer clientPool.Put(client)
+
+	log.Print("Client retrieved from pool")
 
 	bqResp := textsToTexts(ctx, client, bqReq)
 	SendSuccess(w, bqResp)
